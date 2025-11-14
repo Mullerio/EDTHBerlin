235233fa0cf5d826c3565c2dccec001b384d7b05
PaulProske <jazzsuite1@gmail.com>
2025-11-14 13:18:13 +0100

    wip interceptors

--- src/detectors.py
+++ src/detectors.py
@@ -1,19 +1,19 @@
-import src.detector_configs
+import src.detector_configs as detector_configs
 
 
 class Detector:
-    def __init__(self, type: DetectorType, position: tuple):
+    def __init__(self, type: detector_configs.DetectorType, position: tuple):
         self.type = type
         self.position = position
         
-        if type == DetectorType.RADAR:
-            self.config = RadarDetectorConfig()
-        if type == DetectorType.ACOUSTIC:
-            self.config = AcousticDetectorConfig()
+        if type == detector_configs.DetectorType.RADAR:
+            self.config = detector_configs.RadarDetectorConfig()
+        if type == detector_configs.DetectorType.ACOUSTIC:
+            self.config = detector_configs.AcousticDetectorConfig()
         if type == DetectorType.VISUAL:
-            self.config = VisualDetectorConfig()
-        if type == DetectorType.RADIO:
-            self.config = RadioDetectorConfig()
+            self.config = detector_configs.VisualDetectorConfig()
+        if type == detector_configs.DetectorType.RADIO:
+            self.config = detector_configs.RadioDetectorConfig()
 
         self.radius = self.config.radius
         self.probability_function = self.config.probability
@@ -21,7 +21,7 @@ class Detector:
     def probability(self, distance: float) -> float:
         return self.probability_function(distance)
 
-    def detect(self, target: Target) -> bool:
+    def detect(self, target) -> bool:
         return self.probability(target.distance) > 0.5
 
     
---
+++ src/interceptors.py
@@ -0,0 +1,20 @@
+import configs.interceptor_configs as interceptor_configs
+
+class InterceptorUnit:
+    def __init__(self, type: interceptor_configs.InterceptorType, position: tuple, capacity = None):
+        self.type = type
+        self.position = position
+    
+        if type == interceptor_configs.InterceptorType.A:
+            self.config = interceptor_configs.InterceptorConfigA()
+        if type == interceptor_configs.InterceptorType.B:
+            self.config = interceptor_configs.InterceptorConfigB()
+
+        self.velocity = self.config.velocity
+        self.time_to_start = self.config.time_to_start
+
+        if capacity is None:
+            self.capacity = self.config.capacity
+        else:
+            self.capacity = capacity
+
